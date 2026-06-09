35beac7a415aabacc55af774c4b048d61094e169
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-06-09 02:02:42 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,6 @@ dependencies {
     implementation 'com.google.android.material:material:1.9.0'
     implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
 
-    // Logging
-    implementation 'com.squareup.okhttp3:logging-interceptor:4.10.0'
-
     testImplementation 'junit:junit:4.13.2'
     androidTestImplementation 'androidx.test.ext:junit:1.1.5'
     androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
